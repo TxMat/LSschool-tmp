14dc67d8a6d08a48e48371c910ef957f0d3508f4
ponalm <mathieu.ponal@etu.univ-grenoble-alpes.fr>
2022-04-12 18:56:01 +0200

    ez

--- app/src/main/java/com/example/berneteprojetandroid/CultureGeneraleActivity.java
+++ app/src/main/java/com/example/berneteprojetandroid/CultureGeneraleActivity.java
@@ -2,13 +2,50 @@ package com.example.berneteprojetandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import com.example.berneteprojetandroid.bdd.BD;
+import com.example.berneteprojetandroid.bdd.Questionnaire;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CultureGeneraleActivity extends AppCompatActivity {
+
+    private ArrayList<Questionnaire> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_culture_generale);
+    }
+
+    private void getQuestions() {
+        ///////////////////////
+        // Classe asynchrone permettant de récupérer des taches et de mettre à jour le listView de l'activité
+        class GetQuestions extends AsyncTask<Void, Void, List<Questionnaire>> {
+
+            @Override
+            protected List<Questionnaire> doInBackground(Void... voids) {
+                return BD.getInstance(getApplicationContext()).questDao().getAll();
+            }
+
+            @Override
+            protected void onPostExecute(List<Questionnaire> qz) {
+                super.onPostExecute(qz);
+                // la var est pleine mtn
+                questionList = (ArrayList<Questionnaire>) qz;
+
+                // le code pour quand question list est ok va la
+
+                //int questionnb = getIntent().getIntExtra("numberOfQuestions", 10);
+                //quizModel = new QuizModel(questionnb, questionList);
+                //initialize();
+            }
+        }
+
+        GetQuestions gq = new GetQuestions();
+        gq.execute();
     }
 }
